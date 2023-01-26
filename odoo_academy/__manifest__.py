a7df69978f4745118af78b49bdb8de2f977a606a
stal@odoo.com <odoo@eupd04.odoo.com>
2023-01-26 20:57:10 +0000

    [ADD] Basic Module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Academy',
+    'summary': """Academy app to manage Training""",
+    
+    'description': """
+        Academy Module to Manage Training:
+            -Courses
+            -Sessions
+            -Attendees
+     """,
+    
+    'author':'Odoo',
+    
+    'website':'https://www.odoo.com',
+    
+    'category': 'Training',
+    'version':'0.1',
+    
+    'depends': ['base'],
+    
+    'data': [ 
+        
+    ],
+    'demo': [
+    
+    ],
+}
