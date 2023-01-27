b0e8c3f6946969963fd1ad00a3bcd4ef49349c43
stal-odoo <stal@odoo.com>
2023-01-27 19:59:48 +0000

    [ADD] Basic Module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Odoo Academy',
+    'summary': """Academy app to manage Training""",
+
+    'description': """
+        Academy Module to manage Training:
+        - Courses
+        - Sessions
+        - Attendees
+        """,
+    'license': 'OPL-1',
+    'author': 'fsrs-odoo',
+    'website': 'www.odoo.com',
+    'category': 'Tech Training',
+    
+    'depends': ['base'],
+    'data': [],
+  
+}
